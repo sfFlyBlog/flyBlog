68147ee0694879e195c3fb834acc0ae2d3309642
“sfFlyBlog” <13571673731@163.com>
2020-04-13 23:00:16 +0800

    配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,110 @@
 module.exports = {
     base: '/',
-    title: 'fly的前端学习之路',
-    description: '博客',
+    title: '前端踩坑笔记',
+    description: '努力的人总是幸运的',
     themeConfig: {
         // 你的GitHub仓库，请正确填写
         repo: 'https://github.com/xxxxxxx/blog-demo',
         // 自定义仓库链接文字。
         repoLabel: 'GitHub',
-        sidebar: [
-            ['/', '首页'],
-            ['/blog/FirstBlog.md', '我的第一篇博客']
-        ]
+        nav:[ // 导航栏配置
+              
+            {
+              text: '基础学习',
+              items: [
+                  { text: 'html知识点总结', link: '/html/' },
+                  { text: 'css知识点总结', link: '/css/' },
+                  { text: 'js知识点总结', link: '/js/' },
+                  { text: 'ts知识点总结', link: '/ts/' },
+              ]
+          },
+          {
+            text: '框架系列',
+            items: [
+                { text: 'vue知识点总结和源码分析', link: '/vue/' },
+                { text: 'react知识点总结和远吗分析', link: '/react/' },
+            ]
+          },
+          {
+            text: '构建工具',
+            items: [
+                { text: 'webpack', link: '/webpack/' },
+            ]
+          },
+          {
+            text: '小程序',
+            link: '/wechat/',
+          },
+          {
+            text: '浏览器渲染',
+            link: '/chrome/',
+          },
+         
+          {
+            text: 'http知识点',
+            link: '/http/',
+          },
+          {
+            text: '后端学习',
+            items: [
+                { text: 'node', link: '/node/' },
+            ]
+          },
+          {
+            text: '面试题总结',
+            link: '/interview/',
+          },
+          
+        ],
+        sidebar: {
+            '/html/': [
+                {
+                  title:'html知识点总结',
+                  collapsable: false,
+                  children:[
+                    
+                  ]
+                }
+            ],
+            '/css/':[
+                {
+                    title:'css知识点总结',
+                    collapsable: false,
+                    children:[
+                      '',
+                      ''
+                    
+                    ]
+                  }
+            ],
+            '/js/':[
+              {
+                  title:'js知识点总结',
+                  collapsable: false,
+                  children:[
+                    '',
+                  ]
+                }
+          ],
+          '/ts':[
+            {
+                title:'ts知识点总结',
+                collapsable: false,
+                children:[
+                  '',
+                ]
+            },
+          ],
+          '/vue':[
+            {
+                title:'vue知识点总结',
+                collapsable: false,
+                children:[
+                  'hone',
+                ]
+            }
+          ]
+        }
+  
     }
   }
