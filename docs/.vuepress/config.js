dbb3ca1939965457895065235bc5a056586b431e
suofei <13571673731@163.com>
2020-04-13 16:53:47 +0800

    blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    base: '/',
+    title: 'fly的前端学习之路',
+    description: '博客',
+    themeConfig: {
+        // 你的GitHub仓库，请正确填写
+        repo: 'https://github.com/xxxxxxx/blog-demo',
+        // 自定义仓库链接文字。
+        repoLabel: 'GitHub',
+        sidebar: [
+            ['/', '首页'],
+            ['/blog/FirstBlog.md', '我的第一篇博客']
+        ]
+    }
+  }
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /link.gif
+heroText: fly的前端学习之路
+actionText: 快速上手 →
+actionLink: /
+---
